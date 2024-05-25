a1fa63f33157a4f48301ee1339f3da2881fac2dc
Demais13 <erickvianna.d@gmail.com>
2024-05-25 17:14:28 -0300

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace bob {
+    //% block="Heie $v"
+    //% v.defl ="bob"
+    export function heie(v:string): void {
+        led.plot(1,1)
+        basic.showString(v)
+    }
+}
